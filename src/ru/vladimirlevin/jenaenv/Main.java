3f441e148f06732f09bebe493b62a28ff48b6582
Vladimir Levin <Vladimir_Levin@epam.com>
2016-02-09 17:16:47 +0300

    Initial commit

---
+++ src/ru/vladimirlevin/jenaenv/Main.java
@@ -0,0 +1,10 @@
+package ru.vladimirlevin.jenaenv;
+
+/**
+ * Created by Vladimir_Levin on 06.12.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
